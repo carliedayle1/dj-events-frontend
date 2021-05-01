301de70a6d3283c447b3a23a5616199c1c62e9ec
Erick Dayle Loon <daylecarlie@gmail.com>
2021-05-01 20:19:20 +0800

    Ready for production

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import { AuthProvider } from "@/context/AuthContext";
+import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+}
+
+export default MyApp;
